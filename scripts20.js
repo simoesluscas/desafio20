98c7b1e7277be4d27899778c800c4a02d2b35a5f
simoesluscas <132956828+simoesluscas@users.noreply.github.com>
2023-07-10 19:58:40 -0300

    Create scripts20.js

---
+++ scripts20.js
@@ -0,0 +1,13 @@
+var elements = document.getElementsByClassName(names);
+
+function funcaoQueSeraExecutada(){
+    alert("clicou")
+}
+<button onclick="funcaoQueSeraExecutada()">
+    Clique aqui
+</button>
+
+var nomeDaTag = "div";
+var element = document.createElement(nomeDaTag);
+var text = "Hello World";
+var elemento = document.createTextNode(text);
